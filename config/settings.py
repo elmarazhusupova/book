6732ba67d616e344e36826ac2e492faba7b75286
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-03-01 18:13:40 +0600

    add whitenoise

--- config/settings.py
+++ config/settings.py
@@ -48,8 +48,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
