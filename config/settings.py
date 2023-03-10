2f6509e673dbbcdfe04d4fe310d0a3ad09dabdc8
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-03-10 21:36:06 +0600

    add cloudinary to req

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+from cloudinary.templatetags import cloudinary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
